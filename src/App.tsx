e750eef7227e8bb2ffe356afe690744a3a08e8ee
Juan Manuel Alvarez <juan.alvarez6@utp.edu.co>
2025-03-04 11:19:51 -0500

    Agreando tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ function App() {
 
   return (
     <>
-      <h1>Home</h1>
-      <div className='links'>
-          <Link to='/login' className='link'>Login </Link>
-          <Link to='/dashboard' className='link'>Dashboard </Link>
-          <Link to='/worker-panel' className='link'>Worker-panel </Link>
-          <Link to='/material-requests' className='link'>MaterialRequests </Link>
-          <Link to='/task-management' className='link'>TaskManagement </Link>
-          <Link to='/reports' className='link'>Reports</Link>
+      <h1 className='text-3xl font-bold m-8'>Home</h1>
+      <div className='flex justify-around m-8'>
+          <Link to='/login' className='bg-blue-500 rounded-full p-3 hover:bg-sky-300'>Login </Link>
+          <Link to='/dashboard' className='bg-blue-500 rounded-full p-3 hover:bg-sky-300'>Dashboard </Link>
+          <Link to='/worker-panel' className='bg-blue-500 rounded-full p-3 hover:bg-sky-300'>Worker-panel </Link>
+          <Link to='/material-requests' className='bg-blue-500 rounded-full p-3 hover:bg-sky-300'>MaterialRequests </Link>
+          <Link to='/task-management' className='bg-blue-500 rounded-full p-3 hover:bg-sky-300'>TaskManagement </Link>
+          <Link to='/reports' className='bg-blue-500 rounded-full p-3 hover:bg-sky-300'>Reports</Link>
       </div>
     </>
   )
